8e168928a66c44f97678f712f127cb4b34e92e73
e1b21072 <e1b21072@oit.ac.jp>
2023-10-24 14:01:47 +0900

    first commit

---
+++ src/main/java/obanyan/frunyan/FrunyanApplication.java
@@ -0,0 +1,13 @@
+package obanyan.frunyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrunyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrunyanApplication.class, args);
+	}
+
+}
