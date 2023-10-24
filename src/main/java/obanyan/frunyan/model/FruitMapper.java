124f2cd88c88c9cc41cf40636e37f7fd482286df
e1b21115 <119831972+e1b21115@users.noreply.github.com>
2023-10-24 14:53:37 +0900

    Fruitテーブルの一覧を表示

---
+++ src/main/java/obanyan/frunyan/model/FruitMapper.java
@@ -0,0 +1,15 @@
+package obanyan.frunyan.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface FruitMapper {
+
+  @Select("SELECT ID, NAME,PRICE FROM FRUIT")
+  ArrayList<Fruit> selectAllFruit();
+}
